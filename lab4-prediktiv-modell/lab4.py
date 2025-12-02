beb68caa95a43dc7d6fcb16a673fe4e2fe0b76ae
patriciaanna6 <baladjaypatricia6@gmail.com>
2025-12-02 10:51:14 +0100

    lab 2 & lab 4

---
+++ lab4-prediktiv-modell/lab4.py
@@ -0,0 +1,12 @@
+# STEG 1: Importera bibliotek
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# För machine learning
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+print(" Bibliotek importerade!")
+print("Vi använder sklearn - världens enklaste ML-bibliotek")
